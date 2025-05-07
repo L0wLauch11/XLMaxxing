2106bb6182294878ab20d643451823b4eabdb589
L0wLauch11 <42972587+L0wLauch11@users.noreply.github.com>
2025-05-07 22:57:13 +0200

    feat: avoid executing already done migrations

--- database/scripts/migrate.php
+++ database/scripts/migrate.php
@@ -7,6 +7,7 @@ if (PHP_SAPI !== 'cli' || isset($_SERVER['HTTP_USER_AGENT'])) {
 $rootFolder = __DIR__.'/../..';
 $databaseFolder = $rootFolder.'/database';
 $migrationsFolder = $rootFolder.'/database/migrations';
+$migratedLogPath = "$migrationsFolder/migrated.log";
 
 require_once __DIR__.'/../../setup.php';
 
@@ -19,14 +20,25 @@ $migrations = [
     '2025-05-07_DEV_populate_xlfiles'
 ];
 
+$migrationCount = 0;
+
 foreach ($migrations as $migration) {
     $migrationEnvMode = str_contains($migration, "DEV") ? "DEV" : "PROD";
-    if ($migrationEnvMode == "DEV" && !Env::DEV_ENV) {
+    if (($migrationEnvMode == "DEV" && !Env::DEV_ENV)
+            || str_contains(file_get_contents($migratedLogPath), $migration)) {
         continue;
     }
 
+    $migratedLog = fopen($migratedLogPath, 'w');
+    fwrite($migratedLog, "$migration,");
+    fclose($migratedLog);
+
     $database->query(file_get_contents("$migrationsFolder/$migration.sql"));
     print("$migrationEnvMode migration `$migration.sql` executed.\n");
-}
 
-print("Migration finished!\n");
+    $migrationCount++;
+}
+print(
+    ($migrationCount > 0)
+        ? "Migration finished ($migrationCount)!\n" : "Nothing to migrate!"
+);
